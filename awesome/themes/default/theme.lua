999d419307b7da7e1638d03bda24f09e74e29c9d
Biel <bielp07@gmail.com>
2020-06-07 08:55:59 -0300

    Update theme.lua

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -15,8 +15,8 @@ local theme = {}
 
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_normal     = "#000000"
+theme.bg_focus      = "#222222"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
@@ -29,7 +29,7 @@ theme.fg_minimize   = "#ffffff"
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+theme.border_focus  = "#ff0000"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
